6e354f7d7b7029568f8fe90ef7266c3afa49f59b
Konstantinos Stamatakis <kostarasae@gmail.com>
2026-03-16 13:47:37 +0200

    chore: add authentication service

---
+++ src/main/java/gr/aueb/cf/restbankapp/authentication/AuthenticationService.java
@@ -0,0 +1,30 @@
+package gr.aueb.cf.restbankapp.authentication;
+
+import gr.aueb.cf.restbankapp.dto.AuthenticationRequestDTO;
+import gr.aueb.cf.restbankapp.dto.AuthenticationResponseDTO;
+import gr.aueb.cf.restbankapp.model.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponseDTO authenticate(AuthenticationRequestDTO dto) {
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(dto.username(), dto.password()));
+
+        User user = (User) authentication.getPrincipal();
+
+        String token = jwtService.generateToken(authentication.getName(), user.getRole().getName());
+
+        return new AuthenticationResponseDTO(token);
+    }
+}
