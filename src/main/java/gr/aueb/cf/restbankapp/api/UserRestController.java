83ef7c23a1211bca83c28ab371399b87eb91d751
Konstantinos Stamatakis <kostarasae@gmail.com>
2026-03-16 13:40:03 +0200

    chore: add user controller

---
+++ src/main/java/gr/aueb/cf/restbankapp/api/UserRestController.java
@@ -0,0 +1,64 @@
+package gr.aueb.cf.restbankapp.api;
+
+import gr.aueb.cf.restbankapp.core.exceptions.EntityAlreadyExistsException;
+import gr.aueb.cf.restbankapp.core.exceptions.EntityInvalidArgumentException;
+import gr.aueb.cf.restbankapp.core.exceptions.EntityNotFoundException;
+import gr.aueb.cf.restbankapp.core.exceptions.ValidationException;
+import gr.aueb.cf.restbankapp.dto.RoleReadOnlyDTO;
+import gr.aueb.cf.restbankapp.dto.UserInsertDTO;
+import gr.aueb.cf.restbankapp.dto.UserReadOnlyDTO;
+import gr.aueb.cf.restbankapp.service.IRoleService;
+import gr.aueb.cf.restbankapp.service.IUserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.UUID;
+
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+public class UserRestController {
+
+    private final IUserService userService;
+
+
+    @PostMapping
+    public ResponseEntity<UserReadOnlyDTO> registerUser(@Valid @RequestBody UserInsertDTO userInsertDTO,
+                                       BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model)
+            throws ValidationException, EntityAlreadyExistsException, EntityInvalidArgumentException {
+
+        // user validator business rules TODO
+
+        if (bindingResult.hasErrors()) {
+            throw new ValidationException("User", "Invalid user data", bindingResult);
+        }
+
+        UserReadOnlyDTO userReadOnlyDTO = userService.saveUser(userInsertDTO);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{uuid}")
+                .buildAndExpand(userReadOnlyDTO.uuid())
+                .toUri();
+
+        return ResponseEntity
+                .created(location)
+                .body(userReadOnlyDTO);
+    }
+
+    @GetMapping("/{uuid}")
+    public ResponseEntity<UserReadOnlyDTO> getUserByUUID(@PathVariable UUID uuid)
+            throws EntityNotFoundException {
+
+        return ResponseEntity.ok(userService.getUserByUUIDDeletedFalse(uuid));
+    }
+
+}
