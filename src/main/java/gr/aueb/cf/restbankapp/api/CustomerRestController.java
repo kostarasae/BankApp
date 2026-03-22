d9966c757266269cfbf6ff05e194aad289cc4b92
Konstantinos Stamatakis <kostarasae@gmail.com>
2026-03-22 22:56:20 +0200

    chore: add update getFiltered getByUuid delete customer controller

--- src/main/java/gr/aueb/cf/restbankapp/api/CustomerRestController.java
+++ src/main/java/gr/aueb/cf/restbankapp/api/CustomerRestController.java
@@ -1,23 +1,25 @@
 package gr.aueb.cf.restbankapp.api;
 
-import gr.aueb.cf.restbankapp.core.exceptions.EntityAlreadyExistsException;
-import gr.aueb.cf.restbankapp.core.exceptions.EntityInvalidArgumentException;
-import gr.aueb.cf.restbankapp.core.exceptions.ValidationException;
+import gr.aueb.cf.restbankapp.core.exceptions.*;
+import gr.aueb.cf.restbankapp.core.filters.CustomerFilters;
 import gr.aueb.cf.restbankapp.dto.CustomerInsertDTO;
 import gr.aueb.cf.restbankapp.dto.CustomerReadOnlyDTO;
+import gr.aueb.cf.restbankapp.dto.CustomerUpdateDTO;
 import gr.aueb.cf.restbankapp.service.ICustomerService;
 import gr.aueb.cf.restbankapp.validation.CustomerInsertValidator;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/v1/customers")
@@ -50,6 +52,56 @@ public class CustomerRestController {
         return ResponseEntity
                 .created(location)
                 .body(customerReadOnlyDTO);
+    }
 
+    @PutMapping("/{uuid}")
+    public ResponseEntity<CustomerReadOnlyDTO> updateCustomer(
+            @PathVariable UUID uuid,
+            @Valid @RequestBody CustomerUpdateDTO customerUpdateDTO,
+            BindingResult bindingResult)
+            throws EntityAlreadyExistsException, EntityInvalidArgumentException, ValidationException, EntityNotFoundException {
+
+        if (bindingResult.hasErrors()) {
+            throw new ValidationException("Customer", "Invalid customer data", bindingResult);
+        }
+
+        //customerUpdateValidator.validate(customerUpdateDTO, bindingResult);
+
+        CustomerReadOnlyDTO customerReadOnlyDTO = customerService.updateCustomer(customerUpdateDTO);
+
+        return ResponseEntity.ok(customerReadOnlyDTO);
+    }
+
+    @PostMapping("/{uuid}/afm-file")
+    public ResponseEntity<Void> uploadAfmFile(
+            @PathVariable UUID uuid,
+            @RequestParam("afmFile") MultipartFile afmFile
+    ) throws EntityNotFoundException, FileUploadException {
+
+        customerService.saveAfmFile(uuid, afmFile);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<CustomerReadOnlyDTO>> getFilteredAndPaginatedTeachers(
+            @PageableDefault(page = 0, size = 5) Pageable pageable,
+            @ModelAttribute CustomerFilters filters // instantiates filters with NoArgsConstructor
+            ) throws EntityNotFoundException {
+        Page<CustomerReadOnlyDTO> paginatedDTO = customerService.getCustomersPaginatedFiltered(pageable, filters);
+        return ResponseEntity.ok(paginatedDTO);
+    }
+
+    @GetMapping("/{uuid}")
+    public ResponseEntity<CustomerReadOnlyDTO> getCustomerByUUID(@PathVariable UUID uuid)
+        throws EntityNotFoundException {
+        CustomerReadOnlyDTO customerReadOnlyDTO = customerService.getCustomerByUUIDDeletedFalse(uuid);
+        return ResponseEntity.ok(customerReadOnlyDTO);
+    }
+
+    @DeleteMapping("/{uuid}")
+    public ResponseEntity<CustomerReadOnlyDTO> deleteCustomerByUUID(@PathVariable UUID uuid)
+            throws EntityNotFoundException {
+        CustomerReadOnlyDTO customerReadOnlyDTO = customerService.deleteCustomerByUUID(uuid);
+        return ResponseEntity.ok(customerReadOnlyDTO);
     }
 }
