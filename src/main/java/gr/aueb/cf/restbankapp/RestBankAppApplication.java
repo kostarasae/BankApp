74e5f540eb10fc63e6986b8dec1b03db91557f54
Konstantinos Stamatakis <kostarasae@gmail.com>
2026-03-22 21:30:04 +0200

    chore: add enable retry and async

--- src/main/java/gr/aueb/cf/restbankapp/RestBankAppApplication.java
+++ src/main/java/gr/aueb/cf/restbankapp/RestBankAppApplication.java
@@ -3,9 +3,18 @@ package gr.aueb.cf.restbankapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+import java.math.BigDecimal;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableRetry
+@EnableAsync
 public class RestBankAppApplication {
 
 	public static void main(String[] args) {
